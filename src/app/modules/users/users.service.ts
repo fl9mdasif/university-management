a0e57b2406620063054a1e95ae315b72eff8a584
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2023-06-08 13:02:12 +0600

    finished errorHandelr global function

--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -2,11 +2,13 @@ import config from '../../../config/index'
 import { IUser } from './users.interface'
 import { User } from './user.model'
 import { generateUserId } from './users.utils'
+import ApiError from '../../../errors/ApiError'
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   // auto generated incremental id
   const id = await generateUserId()
   user.id = id
+
   // default password
   if (!user.password) {
     user.password = config.default_user_password as string
@@ -15,7 +17,7 @@ const createUser = async (user: IUser): Promise<IUser | null> => {
   const createdUser = await User.create(user)
 
   if (!createUser) {
-    throw new Error('Failed to create user!')
+    throw new ApiError(400, 'Failed to create user!', '')
   }
   return createdUser
 }
