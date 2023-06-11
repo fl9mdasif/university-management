a5e43258bfe587693f5400f2294d337dca2e08c4
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2023-06-11 12:55:41 +0600

    [done]- setup zod && zod error handler

---
+++ src/app/middlewares/valildateRequest.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+import { AnyZodObject } from 'zod';
+// import { UserService } from '../modules/user/user.service'
+
+const validateRequest =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+        query: req.query,
+        cookies: req.cookies,
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+export default validateRequest;
